2294865be8e806eac084086ac8415a68bb8b390a
Anshuman1609 <anshumandubey1609@gmail.com>
2024-03-02 17:36:57 +0530

    commit

--- src/components/News.js
+++ src/components/News.js
@@ -8,22 +8,52 @@ export class News extends Component {
         this.state = {
             articles: [],
             loading: false,
-            page : 1
+            page : 1,
+
         }
     }
 
     async componentDidMount(){
-        let url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=8b8b72eea7ed4aa69fd61b85158342b3";
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8b8b72eea7ed4aa69fd61b85158342b3&page=1&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         console.log(parsedData);
-        this.setState({articles : parsedData.articles});
+        this.setState({articles : parsedData.articles , totalResults : parsedData.totalResults});
+    }
+
+    handlePrevClick = async () => {
+        console.log("prev");
+        let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8b8b72eea7ed4aa69fd61b85158342b3&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
+        let data = await fetch(url);
+        let parsedData = await data.json();
+        console.log(parsedData);
+        this.setState({
+            page : this.state.page - 1,
+            articles : parsedData.articles
+        })
+    }
+    
+    handleNextClick = async () => {
+        console.log("next");
+        if(this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)){
+
+        }
+        else{                              
+            let url = `https://newsapi.org/v2/top-headlines?country=in&apiKey=8b8b72eea7ed4aa69fd61b85158342b3&page=${this.state.page  + 1}&pageSize=${this.props.pageSize}`;
+            let data = await fetch(url);
+            let parsedData = await data.json();
+            console.log(parsedData);
+            this.setState({
+                page : this.state.page + 1,
+                articles : parsedData.articles
+            })
+        }
     }
 
     render() {
         return (
             <div className="container my-3">
-                <h2>NewsPanda - Top Headlines</h2>
+                <h1 className="text-center">NewsPanda - Top Headlines</h1>
                 <div className="row">
                     {this.state.articles.map((element) => {
                         return <div className="col-md-4" key={element.url}>
@@ -32,10 +62,13 @@ export class News extends Component {
                         </div>
                     })}
                 </div>
-                
+                <div className="container d-flex justify-content-between">
+                    <button disabled={this.state.page<=1} type="button" className="btn btn-dark" onClick={this.handlePrevClick}> &larr; Previous</button>
+                    <button disabled={this.state.page + 1 > Math.ceil(this.state.totalResults/this.props.pageSize)} type="button" className="btn btn-dark" onClick={this.handleNextClick}>Next &rarr;</button>
+                </div>
             </div>
         )
     }
 }
 
-export default News
+export default News;                
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -4,13 +4,13 @@ export class NewsItem extends Component {
     render() {
         let {title, description, imageUrl, newsUrl} = this.props;
         return (
-            <div className="my-3">
+            <div className="container my-3">
                 <div className="card" style={{width: "18rem"}}>
                     <img src={imageUrl ? imageUrl : "https://www.livemint.com/lm-img/img/2024/03/01/1600x900/xiaomi_14_ultra_1708671913109_1709272035612.jpg" } className="card-img-top" alt="..." />
                         <div className="card-body">
                             <h5 className="card-title">{title}...</h5>
                             <p className="card-text">{description}...</p>
-                            <a href={newsUrl} target='_blank' rel='noreferrer' className="btn btn-sm btn-primary">Read More</a>
+                            <a href={newsUrl} target='_blank' rel='noreferrer' className="btn btn-sm btn-dark">Read More</a>
                         </div>
                 </div>
             </div>
